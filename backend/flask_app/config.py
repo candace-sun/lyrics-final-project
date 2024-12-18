575b2e0c76e2cac59aea61b60b35cc7cc9070694
Candace <csun0@terpmail.umd.edu>
2024-12-18 15:58:53 -0500

    more changes

---
+++ backend/flask_app/config.py
@@ -0,0 +1,6 @@
+# Stores all configuration values
+import os
+
+SECRET_KEY = os.enivron.get('SECRET_KEY') 
+MONGODB_HOST = os.enivron.get('MONGODB_HOST') 
+LASTFM_KEY = os.enivron.get('LASTFM_KEY') 
